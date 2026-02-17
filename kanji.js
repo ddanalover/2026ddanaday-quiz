6327647352dd54d4b92ddb5f9a64733bd25e0248
ddanalover <my.sweetie.ddana@gmail.com>
2026-02-17 19:33:23 +0900

    Delete kanji.js

--- kanji.js
+++
@@ -1,9 +0,0 @@
-window.masterDB = window.masterDB || {};
-window.masterDB.kanji = [
-  {
-    q: "<ruby>湯<rt>ゆ</rt></ruby>의 뜻은?",
-    o: ["뜨거운 물", "차가운 물"],
-    a: 0,
-    e: "「湯」는 ‘뜨거운 물’이라는 뜻입니다."
-  }
-];
